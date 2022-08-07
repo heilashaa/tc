9bb79b6481a0745d1a6408c5415ea9940d8063cb
a.geylash <a.geylash@itransition.com>
2022-08-07 20:47:43 +0300

    second-1 add comments

--- src/main/java/com/example/tc/controller/HelloController.java
+++ src/main/java/com/example/tc/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Rest controller.
+ */
 @RestController
 public class HelloController
 {
